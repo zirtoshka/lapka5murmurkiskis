e21f0137effea93a159ff8e072d67ae48ab2ea2d
zirtoshka <367222@edu.itmo.ru>
2023-03-12 03:05:18 +0300

    исправила константы2.0

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,29 +14,30 @@ import java.util.Scanner;
 public class Main {
     public static final String INPUT_COMMAND = "$ ";
     public static final String INPUT_INFO = "> ";
+    public static final int numberOfCmd = 12;
 
     public static void main(String[] args) throws IOException, ArrayIndexOutOfBoundsException {
         Scanner userScanner = new Scanner(System.in);
         HistoryWriter historyWriter = new HistoryWriter();
         ScannerManager scannerManager = new ScannerManager(userScanner);
-        FileManager fileManager=new FileManager(new Scanner(System.in).nextLine());
-         try {
+        FileManager fileManager = new FileManager(new Scanner(System.in).nextLine());
+        try {
 //             FileManager fileManager = new FileManager(args[0]);
 
-        CollectionManager collectionManager = new CollectionManager(fileManager);
-        collectionManager.loadFromFile();
-        CommandManager commandManager = new CommandManager(
-                new HelpCommand(), new InfoCommand(collectionManager), new ShowCommand(collectionManager),
-                new AddCommand(collectionManager, scannerManager), new UpdateByIdCommand(collectionManager, scannerManager),
-                new RemoveByIdCommand(collectionManager), new ClearCommand(collectionManager), new SaveCommand(collectionManager),
-                new ExecuteScriptCommand(), new ExitCommand(), new HeadCommand(collectionManager), new AddIfMaxCommand(collectionManager, scannerManager),
-                new HistoryCommand(historyWriter), new FilterContainsNameCommand(collectionManager), new PrintUniqueGroupAdminCommand(collectionManager),
-                new PrintFieldDescendingSemesterCommand(collectionManager));
-        ConsoleManager consoleManager = new ConsoleManager(commandManager, scannerManager, userScanner, historyWriter);
-        consoleManager.toStartMode();}
-         catch (ArrayIndexOutOfBoundsException e){
-             ConsoleManager.printError("I can't find name of file");
-         }
+            CollectionManager collectionManager = new CollectionManager(fileManager);
+            collectionManager.loadFromFile();
+            CommandManager commandManager = new CommandManager(
+                    new HelpCommand(), new InfoCommand(collectionManager), new ShowCommand(collectionManager),
+                    new AddCommand(collectionManager, scannerManager), new UpdateByIdCommand(collectionManager, scannerManager),
+                    new RemoveByIdCommand(collectionManager), new ClearCommand(collectionManager), new SaveCommand(collectionManager),
+                    new ExecuteScriptCommand(), new ExitCommand(), new HeadCommand(collectionManager), new AddIfMaxCommand(collectionManager, scannerManager),
+                    new HistoryCommand(historyWriter, numberOfCmd), new FilterContainsNameCommand(collectionManager), new PrintUniqueGroupAdminCommand(collectionManager),
+                    new PrintFieldDescendingSemesterCommand(collectionManager));
+            ConsoleManager consoleManager = new ConsoleManager(commandManager, scannerManager, userScanner, historyWriter);
+            consoleManager.toStartMode();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            ConsoleManager.printError("I can't find name of file");
+        }
 
 
     }
